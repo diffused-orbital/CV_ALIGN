802eff32a56367580247aef59ef062cdbf1d6a80
Sanidhya Srivastava <sanidhya3011@gmail.com>
2025-06-09 18:05:21 +0530

    Frontend structure setup

---
+++ Frontend/frontend/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div>
+      {/* Your form JSX here */}
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
---
+++ Frontend/frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import "C:/CV-Align/Frontend/frontend/src/styles/Global.css"
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
