2adbcab5b6210b9d8314a75add617de04258ddba
T-arn21 <arnavtiku@gmail.com>
2025-06-27 21:38:23 +0530

    connecting backend and frontend

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/login';
 import Register from './pages/register';  
 import CandidateDashboard from './pages/candidatedashboard';
 
+
 export default function App() {
   return (
     <Routes>
